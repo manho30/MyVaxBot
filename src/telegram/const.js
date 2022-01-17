e7f1d1a8562834f3bf49d4baef649d93f6e252f7
manho <86037446+manho30@users.noreply.github.com>
2022-01-17 14:33:40 +0800

    Delete src directory

--- src/telegram/const.js
+++
@@ -1,6 +0,0 @@
-// change the only bot token and gasLink 
-var Const = {}
-Const.botToken = "<bot_token>"
-Const.myId = 12345678
-Const.channel = "<Channel>"
-Const.gasLink = "<google_deployed_link>" 
--- src/telegram/webhook.js
+++
@@ -1,5 +0,0 @@
-/**Execute this fetchingWebhook function once you get the gas link **/
-function fetchingWebhook() {
-    var res = UrlFetchApp.fetch('api.telegram.org/bot' + Const.botToken + '/setWebhook?url=' + Const.gasLink)
-    Logger.log(res)
-}
