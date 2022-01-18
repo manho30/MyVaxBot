1df461cca1b692b6ce3e258b8aff65c18fc76e6e
manho <86037446+manho30@users.noreply.github.com>
2022-01-18 19:13:03 +0800

    Update main.js

--- function/src/main.js
+++ function/src/main.js
@@ -20,14 +20,14 @@ const bot = (body) => {
             return sendMessage(body.message.chat.id, "Hi, I am a bot can tell you about the number who have been completed vaccination in Malaysia everyday!\nSimply click on /state");
         }
         if (body.message.text.indexOf("/state") === 0) {
-            return sendMessage(body.message.chat.id, getFormatVaccination());
+            return sendMessage(body.message.chat.id, getFormatVaccination(parseVaccination(getVaccination())));
         }
         if (body.message.text.indexOf("/info") === 0) {
             return sendMessage(body.message.chat.id, "I am a bot making count for daily vaccine count in Malaysia🇲🇾.\nAll the data source are from [CITF-Malaysis/citf-public](https://github.com/CITF-Malaysia/citf-public).\nA thousand thanks to [this hero](https://github.com/limhenry).\n\nSource code:\nhttps://github.com/manho30/MyVaxCountBot.\nDeveloped by [@manho30](tg://user?id=" + Const.myId + ").");
         }
         if (body.message.text.indexOf("/school") === 0) {
             if (param[1]) {
-                return sendMessage(body.message.chat.id, getFormatSchoolVaccination(param[1].toUpperCase()));
+                return sendMessage(body.message.chat.id, getFormatSchoolVaccination(parseSchoolVaccination(getSchoolVaccination(), param[1]), param[1].toUpperCase()));
             } else {
                 return sendMessage(body.message.chat.id, "Must entered the school code to find.\nExample: `/school KBC3066`")
             }
@@ -35,7 +35,7 @@ const bot = (body) => {
         if (body.message.text.indexOf("/history") === 0) {
             if (param[1]) {
                 if (checkDate(param[1])) {
-                    return sendMessage(body.message.chat.id, "*Vaccination:*\n\n" + getFormatVaccination(param[1]) + "\n\n\n*Comfirmed case:*\n\n" + getFormatCase(param[1])) ;
+                    return sendMessage(body.message.chat.id, "*Vaccination:*\n\n" + getFormatVaccination(parseVaccination(getVaccination(), param[1]), param[1]) + "\n\n\n*Comfirmed case:*\n\n" + getFormatCase(parseCase(getCases(), param[1]), param[1]));
                 } else {
                     return sendMessage(body.message.chat.id, `${body.message.text.replace("/history", "")} is not an valid date.`)
                 }
@@ -44,7 +44,7 @@ const bot = (body) => {
             }
         }
         if (body.message.text.indexOf("/case") === 0) {
-            return sendMessage(body.message.chat.id, getFormatCase())
+            return sendMessage(body.message.chat.id, getFormatCase(parseCase(getCases())))
         }
     } else {
         return sendMessage(body.message.chat.id, "⛔MY vaccine Bot is temporary stopped now.")
