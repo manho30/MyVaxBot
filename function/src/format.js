f2f0de6c9a3bb73b533963250f2d345602a8bc56
manho <86037446+manho30@users.noreply.github.com>
2022-01-18 19:12:05 +0800

    Update format.js

--- function/src/format.js
+++ function/src/format.js
@@ -1,8 +1,7 @@
-const getFormatCase = (dates) => {
+const getFormatCase = (data, dates) => {
     if(!dates){
         dates = getDates()
     }
-    const data = getCases(dates)
     if(!isNaN(parseInt(data[0].confirmed_caase))){
         const res = "*Confirm cases*: " + parseInt(data[0].confirmed_caase) + "\n" 
                 + "*Import cases*: " + parseInt(data[0].case_import) + "\n" 
@@ -14,8 +13,7 @@ const getFormatCase = (dates) => {
     } 
 }
 
-const getFormatSchoolVaccination = (code) => {
-    const data = getSchoolVaccination(code);
+const getFormatSchoolVaccination = (data) => {
     if (data[0].school_name) {
         const res = generateMarkdownLink(data[0].school_name, data[0].lat + "," + data[0].lng)
                 + " (" + data[0].school_code + ")" + "\n\n" 
@@ -32,22 +30,23 @@ const getFormatSchoolVaccination = (code) => {
     }
 }
 
-const getFormatVaccination = (dates) => {
+const getFormatVaccination = (data, dates) => {
     if (!dates) {
         dates = getDates()
     }
-    const data = getVaccination(dates)
     if(!isNaN(parseInt(data[0].total_dose_2))){
         const results = (data) => {
-            const  fully_percent = "*Fully Vaccinated* (" + vaccinatedPercentage(parseInt(data[0].total_dose_2), population) + ")\n" + generateBar(parseInt(data[0].total_dose_2)) + "\n\n"
-            const  booster_percent = "*Booster Dose* (" + vaccinatedPercentage(parseInt(data[0].total_booster_dose), population) + ")\n" + generateBar(parseInt(data[0].total_booster_dose)) + "\n\n\n"
+            const date = countNeededDay(parseInt(data[0].new_booster_dose), parseInt(data[0].total_booster_dose))
+            const fully_percent = "*Fully Vaccinated* *" + vaccinatedPercentage(parseInt(data[0].total_dose_2), population) + " (+" +  vaccinatedPercentage(parseInt(data[0].new_dose_2), population) + " People*)\n" + generateBar(parseInt(data[0].total_dose_2)) + "\n\n"
+            const booster_percent = "*Booster Dose* *" + vaccinatedPercentage(parseInt(data[0].total_booster_dose), population) + " (+" + vaccinatedPercentage(parseInt(data[0].new_booster_dose), population) + " People*)\n" + generateBar(parseInt(data[0].total_booster_dose)) + "\n\n"
+            const needed_day = `At today rate, it would take approximately ${date} to reach 80% coverage about booster dose.\n\n\n`
             const fully_vaccinated = "*Fully Vaccinated*: " + parseCurrency(Number(data[0].total_dose_2)) + " (+" + parseCurrency(data[0].new_dose_2) + ")\n"
             const first_dose = "*Dose1 Completed*: " + parseCurrency(Number(data[0].total_dose_1)) + " (+" + data[0].new_dose_1 + ")\n"
             const booster_dose = "*Booster Dose*: " + parseCurrency(Number(data[0].total_booster_dose)) + " (+" + parseCurrency(data[0].new_booster_dose) + ")\n"
             const kid_first_dose = "*Child Dose1 *: " + parseCurrency(Number(data[0].total_child_dose_1)) + " (+" + parseCurrency(data[0].new_child_dose_1) + ")\n"
             const kid_fully_vaccinated = "*Child Fully Vaccinated*: " + parseCurrency(Number(data[0].total_child_dose_2)) + " (+" + parseCurrency(data[0].new_child_dose_2) + ")\n"
             const total_dose_given = "*Total Dose Given*: " + parseCurrency(Number(data[0].total_dose_given)) + "\n"
-            const res = fully_percent + booster_percent + fully_vaccinated + first_dose + booster_dose + kid_first_dose + kid_fully_vaccinated + total_dose_given + "\n\nData as " + data[0].date
+            const res = fully_percent + booster_percent + needed_day + fully_vaccinated + first_dose + booster_dose + kid_first_dose + kid_fully_vaccinated + total_dose_given + "\n\nData as " + data[0].date
             return res
         }
         return results(data)
