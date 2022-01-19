53402797e262d6f72250b5b8d6f1ab70bf5bd9d9
manho <86037446+manho30@users.noreply.github.com>
2022-01-19 12:25:39 +0800

    Update auto.js

--- function/src/auto.js
+++ function/src/auto.js
@@ -2,12 +2,12 @@ const channel = "@myvaccinecount";
 const id = Const.myId;
 
 const AutoSendMessage = async () => {
-    const sendVaccination = await sendMessage(id, getFormatVaccination(parseVaccination(getVaccination())));
-    const sendCase = await sendMessage(id,getFormatCase(parseCase(getCases())));
+    const sendVaccination = await sendMessage(channel, getFormatVaccination(parseVaccination(getVaccination())));
+    const sendCase = await sendMessage(channel,getFormatCase(parseCase(getCases())));
     const resVax = await postTelegram(sendVaccination);
     const resCase = await postTelegram(sendCase);
     if(resVax.ok === true){
-        const pinVax = await postTelegram(pinMsg(id, resVax.result.message_id));
+        const pinVax = await postTelegram(pinMsg(channel, resVax.result.message_id));
         Logger.log(`Result:${resVax}\n${pinVax}`)
     }
 };
