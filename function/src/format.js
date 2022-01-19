aa3f8e8b9cb424ebdd4d02db8d22b126b22fb11b
manho <86037446+manho30@users.noreply.github.com>
2022-01-19 12:22:53 +0800

    Update format.js

--- function/src/format.js
+++ function/src/format.js
@@ -3,19 +3,18 @@ const getFormatCase = (data, dates) => {
         dates = getDates()
     }
     if(!isNaN(parseInt(data[0].confirmed_caase))){
-        const res = "*Confirm cases*: " + parseInt(data[0].confirmed_caase) + "\n" 
-                + "*Import cases*: " + parseInt(data[0].case_import) + "\n" 
-                + "*Local cases*: " + parseInt(data[0].case_local) + "\n\n" 
+        return "*Confirm cases*: " + parseCurrency(Number(data[0].confirmed_caase)) + "\n" 
+                + "*Import cases*: " + parseCurrency(Number(data[0].case_import)) + "\n" 
+                + "*Local cases*: " + parseCurrency(Number(data[0].case_local)) + "\n\n" 
                 + "Data as " + data[0].date
-        return res
     } else {
-        return "*Not Found*"
+        return "*Malaysia government didn't provide the complete data.*"
     } 
 }
 
 const getFormatSchoolVaccination = (data) => {
     if (data[0].school_name) {
-        const res = generateMarkdownLink(data[0].school_name, data[0].lat + "," + data[0].lng)
+        return generateMarkdownLink(data[0].school_name, data[0].lat + "," + data[0].lng)
                 + " (" + data[0].school_code + ")" + "\n\n" 
                 + "*Sumary*\n"
                 + "*Staff (including support staff):*\n"
@@ -24,9 +23,8 @@ const getFormatSchoolVaccination = (data) => {
                 + "*Students (born 2009 or earlier)*:\n" 
                 + "*Fully Vaccinated:* " + toPercent(data[0].all_dose_2) + "\n" 
                 + "*At Least one 1 Dose:* " + toPercent(data[0].all_dose_1) + "\n"
-        return res;
     } else {
-        return "No results found.\nIf your school is not listed, please ask your school's administration to fill in [this template](https://covidnow.moh.gov.my/template_bpswasta.xlsx)and email it to covidnow@moh.gov.my (CC razali.daiman@moe.gov.my) using the subject 'COVIDNOW Inclusion'."
+        return "No results found.\nIf your school is not listed, please ask your school's administration to fill in [this template](https://covidnow.moh.gov.my/template_bpswasta.xlsx) and email it to covidnow@moh.gov.my (CC razali.daiman@moe.gov.my) using the subject 'COVIDNOW Inclusion'."
     }
 }
 
@@ -36,12 +34,12 @@ const getFormatVaccination = (data, dates) => {
     }
     if(!isNaN(parseInt(data[0].total_dose_2))){
         const results = (data) => {
-            const date = countNeededDay(parseInt(data[0].new_booster_dose), parseInt(data[0].total_booster_dose))
-            const fully_percent = "*Fully Vaccinated* *" + vaccinatedPercentage(parseInt(data[0].total_dose_2), population) + " (+" +  vaccinatedPercentage(parseInt(data[0].new_dose_2), population) + " People*)\n" + generateBar(parseInt(data[0].total_dose_2)) + "\n\n"
-            const booster_percent = "*Booster Dose* *" + vaccinatedPercentage(parseInt(data[0].total_booster_dose), population) + " (+" + vaccinatedPercentage(parseInt(data[0].new_booster_dose), population) + " People*)\n" + generateBar(parseInt(data[0].total_booster_dose)) + "\n\n"
-            const needed_day = `At today rate, it would take approximately more ${date} days to reach 80% coverage about booster dose.\n\n\n`
+            const date = countNeededDay(parseInt(data[0].new_booster_dose), Number(data[0].total_booster_dose))
+            const fully_percent = "*Fully Vaccinated* *" + vaccinatedPercentage(Number(data[0].total_dose_2), population) + " (+" +  vaccinatedPercentage(parseInt(data[0].new_dose_2), population) + " People*)\n" + generateBar(parseInt(data[0].total_dose_2)) + "\n\n"
+            const booster_percent = "*Booster Dose* *" + vaccinatedPercentage(Number(data[0].total_booster_dose), population) + " (+" + vaccinatedPercentage(parseInt(data[0].new_booster_dose), population) + " People*)\n" + generateBar(parseInt(data[0].total_booster_dose)) + "\n\n"
+            const needed_day = `As ${data[0].date} rate, it would take approximately more ${date} days to reach 80% coverage about booster dose.\n\n\n`
             const fully_vaccinated = "*Fully Vaccinated*: " + parseCurrency(Number(data[0].total_dose_2)) + " (+" + parseCurrency(data[0].new_dose_2) + ")\n"
-            const first_dose = "*Dose1 Completed*: " + parseCurrency(Number(data[0].total_dose_1)) + " (+" + data[0].new_dose_1 + ")\n"
+            const first_dose = "*Dose1 Completed*: " + parseCurrency(Number(data[0].total_dose_1)) + " (+" +parseCurrency(data[0].new_dose_1) + ")\n"
             const booster_dose = "*Booster Dose*: " + parseCurrency(Number(data[0].total_booster_dose)) + " (+" + parseCurrency(data[0].new_booster_dose) + ")\n"
             const kid_first_dose = "*Child Dose1 *: " + parseCurrency(Number(data[0].total_child_dose_1)) + " (+" + parseCurrency(data[0].new_child_dose_1) + ")\n"
             const kid_fully_vaccinated = "*Child Fully Vaccinated*: " + parseCurrency(Number(data[0].total_child_dose_2)) + " (+" + parseCurrency(data[0].new_child_dose_2) + ")\n"
@@ -51,6 +49,6 @@ const getFormatVaccination = (data, dates) => {
         }
         return results(data)
     } else {
-        return "*Not found*";
+        return "*Malaysia government didn't provide the complete data.*";
     }
 }
