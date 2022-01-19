e1c595cd693fd0241a91d458c24c50210e5fd897
manho <86037446+manho30@users.noreply.github.com>
2022-01-19 11:11:44 +0800

    updated: More clear and objective.

--- function/src/auto.js
+++ function/src/auto.js
@@ -1,9 +1,9 @@
-const channel = "@myvaccinecount"
-const id = Const.myId 
+const channel = "@myvaccinecount";
+const id = Const.myId;
 
 const AutoSendMessage = async () => {
     const sendVaccination = await sendMessage(id, getFormatVaccination(parseVaccination(getVaccination())));
     const sendCase = await sendMessage(id,getFormatCase(parseCase(getCases())));
     const resVax = await postTelegram(sendVaccination);
     const resCase = await postTelegram(sendCase);
-}
+};
