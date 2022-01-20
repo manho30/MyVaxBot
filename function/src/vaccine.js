b712e0ee290a8b52710e5ead8d496af8aa6646e3
manho <86037446+manho30@users.noreply.github.com>
2022-01-20 12:48:57 +0800

    log: changed async function to arrow function.

--- function/src/vaccine.js
+++ function/src/vaccine.js
@@ -1,12 +1,12 @@
-const getVaccination = async () => {
-    return await UrlFetchApp.fetch("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/vaccination/vax_malaysia.csv").toString();
+const getVaccination = () => {
+    return UrlFetchApp.fetch("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/vaccination/vax_malaysia.csv").toString();
 }
 
-const getCases = async () => {
-    return await UrlFetchApp.fetch("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/cases_malaysia.csv").toString();
+const getCases = () => {
+    return UrlFetchApp.fetch("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/cases_malaysia.csv").toString();
 }
 
 
-const getSchoolVaccination = async () => {
-    return await UrlFetchApp.fetch("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/vaccination/vax_school.csv").toString()
+const getSchoolVaccination = () => {
+    return UrlFetchApp.fetch("https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/vaccination/vax_school.csv").toString()
 }
